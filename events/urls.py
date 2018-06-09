e8a5b9d92e825646eb83b85b003cda7e3308dec9
Lucka R <luc.rihova@gmail.com>
2018-06-09 21:12:08 +0200

    Created new table 'events' and model Event (events/models.py)

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('events/', views.event_listing, name='events')
+    path('events/', views.event_listing, name='events'),
+    path('event/<str:name>', views.event_detail, name='detail'),
 ]
